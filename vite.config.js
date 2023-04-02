fb211d12f31b43bc748918e0fb6eeef3aced5793
Cosmitar <mhherrera31@gmail.com>
2023-04-02 04:39:52 +0200

    add glsl and gltf loader plugins

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import gltf from 'vite-plugin-gltf'
 
 // This is required for Vite to work correctly with CodeSandbox
-const server = process.env.APP_ENV === "sandbox" ? { hmr: { clientPort: 443 } } : {};
+const server = process.env.APP_ENV === 'sandbox' ? { hmr: { clientPort: 443 } } : {}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: server,
-  resolve: {
-    alias: {
-      "@src": resolve(__dirname, "./src"),
+export default defineConfig(async configEnv => {
+  const glsl = (await import('vite-plugin-glsl')).default
+  return {
+    server: server,
+    resolve: {
+      alias: {
+        '@src': resolve(__dirname, './src'),
+      },
     },
-  },
-  plugins: [react()],
-});
+    plugins: [react(), glsl(), gltf()],
+  }
+})
